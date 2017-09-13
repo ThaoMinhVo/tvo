65f70b86802c358adbd2970797d0ce0a29cecb19
Thao <thaomvo@gmail.com>
2017-09-12 19:57:44 -0700

    Failed at using vivus so I switched to walkway but still not working. Have to commit to change way of saving SVG.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //new Vivus('tsvg', {duration: 200});
+    //var hi = new Vivus('tsvg', {type: 'scenario-sync', duration: 200, start: 'autostart', forceRender: false, dashGap: 20}, function () {});
+    var svg = new Walkway({
+        selector: 'tsvg',
+        duration: 8000
+    }).draw();
+});
