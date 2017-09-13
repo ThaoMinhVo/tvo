116311fb3dd27f7601270dd29032b2a9dc2a7b32
Thao <thaomvo@gmail.com>
2017-09-12 21:59:44 -0700

    FINALLY got Vivus animation to work on all three SVG separately and have them appear on top of each other and draw one after the other.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     //new Vivus('tsvg', {duration: 200});
     //var hi = new Vivus('tsvg', {type: 'scenario-sync', duration: 200, start: 'autostart', forceRender: false, dashGap: 20}, function () {});
-    var svg = new Walkway({
-        selector: 'tsvg',
-        duration: 8000
-    }).draw();
+    
+    //var svg = new Walkway({
+    //    selector: 'tsvg',
+    //    duration: 8000
+    //}).draw();
+    
+    var obtT = new Vivus('tsvg', {type: 'oneByOne', duration: 500});
+    var obtV = new Vivus('vsvg', {type: 'oneByOne', duration: 500});
+    var obtO = new Vivus('osvg', {type: 'oneByOne', duration: 500});
 });
