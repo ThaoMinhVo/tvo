19cb4eb67af2472ea25e4c510b77bad23df821fc
Thao <thaomvo@gmail.com>
2017-09-14 21:42:49 -0700

    Added two more SVGs to complete first and last name. Figured the new Symbol looked pretty cool as well. Modified animation to accommadate for all letters.

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
     //    duration: 8000
     //}).draw();
     
-    var obtT = new Vivus('tsvg', {type: 'oneByOne', duration: 500});
-    var obtV = new Vivus('vsvg', {type: 'oneByOne', duration: 500});
-    var obtO = new Vivus('osvg', {type: 'oneByOne', duration: 500});
+    var obtT = new Vivus('tsvg', {type: 'sync', duration: 300});
+    var obtH = new Vivus('hsvg', {type: 'sync', duration: 300});
+    var obtA = new Vivus('asvg', {type: 'sync', duration: 300});
+    var obtO = new Vivus('osvg', {type: 'sync', duration: 300});
+    var obtV = new Vivus('vsvg', {type: 'sync', duration: 300});
+    var obtO_last = new Vivus('osvg_last', {type: 'sync', duration: 300});
     
     
     
@@ -21,15 +24,27 @@ $(document).ready(function(){
 
 function introAnimation(){
     $('#T').animate({
-        left: '-=200px',
-        top: '-+50px'
+        left: '-=400px',
+        top: '-=100px'
     });
-    $('#V').animate({
-        left: '+=100px',
-        top: '-+50px'
+    $('#H').animate({
+        left: '-=250px',
+        top: '-=100px'
+    });
+    $('#A').animate({
+        left: '-=170px',
+        top: '-=100px'
     });
     $('#O').animate({
+        left: '-=50px',
+        top: '-=100px'
+    });
+    $('#V').animate({
         left: '+=250px',
-        top: '-+50px'
+        top: '-=100px'
+    });
+    $('#O_last').animate({
+        left: '+=385px',
+        top: '-=100px'
     });
 }
