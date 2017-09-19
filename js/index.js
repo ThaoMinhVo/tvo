f020cd37bfab1fb170bb28bb43caa683463db25b
Thao <thaomvo@gmail.com>
2017-09-19 08:05:20 -0700

    Code cleaning for the index.js file

--- js/index.js
+++ js/index.js
@@ -1,45 +1,28 @@
 $(document).ready(function(){
-    //new Vivus('tsvg', {duration: 200});
-    //var hi = new Vivus('tsvg', {type: 'scenario-sync', duration: 200, start: 'autostart', forceRender: false, dashGap: 20}, function () {});
+    var obtT = new Vivus('tsvg', {type: 'sync', duration: 100}),
+        obtH = new Vivus('hsvg', {type: 'sync', duration: 100}),
+        obtA = new Vivus('asvg', {type: 'sync', duration: 100}),
+        obtO = new Vivus('osvg', {type: 'sync', duration: 100}),
+        obtV = new Vivus('vsvg', {type: 'sync', duration: 100}),
+        obtO_last = new Vivus('osvg_last', {type: 'sync', duration: 100});
     
-    //var svg = new Walkway({
-    //    selector: 'tsvg',
-    //    duration: 8000
-    //}).draw();
+    //$('.object').velocity("transition.flipXIn");
     
-    var obtT = new Vivus('tsvg', {type: 'sync', duration: 100});
-    var obtH = new Vivus('hsvg', {type: 'sync', duration: 100});
-    var obtA = new Vivus('asvg', {type: 'sync', duration: 100});
-    var obtO = new Vivus('osvg', {type: 'sync', duration: 100});
-    var obtV = new Vivus('vsvg', {type: 'sync', duration: 100});
-    var obtO_last = new Vivus('osvg_last', {type: 'sync', duration: 100});
-    
-    $('.object').velocity("transition.flipXIn");
-    
-    
-    
-    obtO.play(function() {
-        // called after the animation completes
-        setTimeout(introAnimation, 500);
-    });
+    // called after the animation completes
+    obtO.play(function() {introAnimation();});
 });
 
 function introAnimation(){
-    expandName();
-    
     //without function() the timmer doesn't work
+    setTimeout(expandName, 500);
     setTimeout(function(){activateTypewriter('typeDeveloper','typeDeveloper', 'span', 2000);}, 1000);
     setTimeout(activateFloral, 3000);
     setTimeout(activateFloralColors, 4000);
     setTimeout(function(){$('#floralOutline').velocity("fadeOut", { duration: 1500 });}, 4500);
-    setTimeout(function(){
-        $('#floralColors').velocity("fadeOut",{ duration: 1500 });
-        $('#typeDeveloper').velocity("fadeOut",{ duration: 1500 });
-    }, 6500);
+    setTimeout(function(){$('#floralColors').velocity("fadeOut",{ duration: 1500 }); $('#typeDeveloper').velocity("fadeOut",{ duration: 1500 });}, 6500);
     setTimeout(collapseName, 8000);
     setTimeout(translateLogo, 8000);
     //setTimeout(function(){$('.animationOverlay').velocity("fadeOut",{ duration: 1500 });}, 9000);
-    
 }
 
 function expandName(){
@@ -96,17 +79,17 @@ function collapseName(){
     });
 }
 
-
-/*used to activate all typewriting text on page and delete blink-caret at end of animation*/
-/*idName  - id of container element of text to animate*/
-/*className  - class to add to container element*/
-/*textElement - element of text (span,div,h1, etc)*/
-/*time - duration of animation in milliseconds so know when to stop blink-caret*/
+/*used to activate all typewriting text on page and delete blink-caret at end of animation
+idName  - id of container element of text to animate
+className  - class to add to container element
+textElement - element of text (span,div,h1, etc)
+time - duration of animation in milliseconds so know when to stop blink-caret*/
 function activateTypewriter(idName, className, textElement, time){
     $('#'+idName).addClass(className);
     setTimeout(function(){$('.'+ className + ' ' + textElement).css('border-right', 'none');}, (time+500));
 }
-    
+
+/*Floral Outline*/
 function activateFloral(){
     $('#floralOutline').show();
     var floralOutline = new Vivus('floralOutline', {type: 'sync', duration: 100});
@@ -117,6 +100,7 @@ function activateFloralColors(){
     $('#floralColors').velocity("fadeIn", { duration: 1500 });
 }
 
+/*Moves logo to the top left hand corner*/
 function translateLogo(){
     $('.object').velocity({
         left: '200px',
